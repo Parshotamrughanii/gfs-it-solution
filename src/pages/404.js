cad2685cb3ecec9ee149b4d204be9a867dd70d70
Parshotam Rughani <Parshotamrughanii.com>
2023-06-12 14:13:05 +0500

    updated

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,9 +3,13 @@ import Image from "next/image";
 import Style from "../styles/PageNotFound.module.css";
 import Link from "next/link";
 import Footer from "../../components/Footer";
+import Head from "next/head";
 const PageNotFound = () => {
   return (
     <div>
+      <Head>
+        <title>Page not Found</title>
+      </Head>
       <div className={Style.notfound}>
         <h1>404</h1>
         <h3>Page Not Found</h3>
