00dee2214a5c6f9bbdd8e4196133dbc76cbc84b0
Parshotam Rughani <Parshotamrughanii.com>
2023-06-13 18:19:10 +0500

    updated cacanonical

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@/styles/globals.css'
-import Head from 'next/head';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@/styles/globals.css";
+import Head from "next/head";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
-  return(
+  const currentPath = useRouter().asPath;
+  const currPath = `https://gfsitsolutions.com/${currentPath}`;
+  return (
     <>
       <Head>
+        <link rel="canonical" href={currPath} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
 
         {/* <meta name="ROBOTS" content="INDEX, FOLLOW" /> */}
-          <meta name="robots" content="noindex,nofollow" />
+        <meta name="robots" content="noindex,nofollow" />
         <script
           type="text/javascript"
           dangerouslySetInnerHTML={{
@@ -29,9 +33,29 @@ export default function App({ Component, pageProps }) {
             `,
           }}
         />
-         <script
-    dangerouslySetInnerHTML={{
-      __html: `
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: `
+          {
+            "@context": "https://schema.org/",
+            "@type": "Organization",
+            "name": "GFS IT Solutions",
+            "url": "https://gfsitsolutions.com/",
+            "sameAs": [
+              "https://www.facebook.com/gfsitsolutions",
+              "https://twitter.com/GFSITSolution",
+              "https://www.instagram.com/gfsitsolutions/",
+              "https://www.youtube.com/@GFSITSOLUTIONS",
+              "https://www.linkedin.com/company/gfs-it-solutions/"
+            ]
+          }
+        `,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
         window.dataLayer = window.dataLayer || [];
         function gtag() {
           dataLayer.push(arguments);
@@ -40,8 +64,8 @@ export default function App({ Component, pageProps }) {
 
         gtag("config", "G-VH84SKZJF2");
       `,
-    }}
-  />
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </>
